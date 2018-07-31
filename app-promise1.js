8ff570ba18b56fee1451b6df0d538a13cf897a50
pattupatidar <education.prateek@gmail.com>
2018-07-31 13:18:03 +0530

    add more information about weather

--- app-promise1.js
+++ app-promise1.js
@@ -24,13 +24,25 @@ axios.get(geocodeUrl).then((response) => {
 
   var lat = response.data.results[0].geometry.location.lat;
   var lng = response.data.results[0].geometry.location.lng;
+
   var weatherUrl = `https://api.darksky.net/forecast/6b7da165dce0a7462558787db88dda80/${lat},${lng}`;
   console.log(response.data.results[0].formatted_address);
   return axios.get(weatherUrl);
 }).then((response) => {
+  var humidity = response.data.currently.humidity;
+  var pressure = response.data.currently.pressure;
+  var windSpeed = response.data.currently.windSpeed;
+  var visibility = response.data.currently.visibility;
+  var summary = response.data.hourly.summary;
+  var icon = response.data.currently.icon;
   var temperature = response.data.currently.temperature;
   var apparentTemperature = response.data.currently.apparentTemperature;
   console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+  console.log(`Humidity: ${humidity}`);
+  console.log(`WindSpeed: ${windSpeed}`);
+  console.log(`visibility: ${visibility}`);
+  console.log(`summary: ${summary}`);
+  console.log(`icon: ${icon}`);
 }).catch((e) => {
   if (e.code === 'ENOTFOUND') {
     console.log('Unable to connect to API servers.');
